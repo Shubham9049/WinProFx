5df1d9c10d4644a81dde46f0fff7a56e12d526ad
unknown <chandanpms@gmail.com>
2025-07-24 15:33:24 +0530

    added stats, aos, and root color

--- components/BenefitsSection.tsx
+++ components/BenefitsSection.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { FaClock, FaGift, FaHeadset, FaShieldAlt } from "react-icons/fa";
+import "../src/app/globals.css";
 
 const cardContent = [
   {
@@ -34,29 +35,32 @@ const cardContent = [
 
 export default function Home() {
   return (
-    <section className="bg-[#0A1119] text-white py-16 px-6 md:px-20">
-      <div className="md:w-11/12 w-5/6 mx-auto grid md:grid-cols-2 gap-10 items-center">
+    <section className=" text-white py-12">
+      <div className="w-11/12 md:w-4/5 mx-auto grid md:grid-cols-2 gap-10 items-center">
         {/* Left Content */}
         <div>
-          <h2 className="text-3xl md:text-4xl font-bold leading-snug">
+          <h2 className="text-2xl md:text-3xl font-bold leading-snug">
             Benefits of <span className="text-cyan-400">Trading</span> With Us
           </h2>
           <p className="text-gray-400 mt-4 max-w-md">
             Discover seamless trading with us, where transparency and advanced
             technology meet for your financial success.
           </p>
-          <button className="mt-6 px-6 py-3 border border-cyan-400 text-cyan-400 rounded-full hover:bg-cyan-400 hover:text-black transition duration-300">
-            Start Trading →
+          <button className="mt-6 relative overflow-hidden px-5 py-2 border border-cyan-400 text-cyan-400 rounded-full group">
+            <span className="relative z-10 transition-colors duration-300 group-hover:text-white">
+              Start Trading →
+            </span>
+            <span className="absolute inset-0 bg-[var(--primary)] translate-x-[-100%] group-hover:translate-x-0 transition-transform duration-300 ease-in-out z-0"></span>
           </button>
         </div>
 
         {/* Right Flip Cards */}
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
           {cardContent.map((card, index) => (
-            <div key={index} className="group [perspective:1000px] w-full h-60">
+            <div key={index} className="group [perspective:1000px] w-full h-52">
               <div className="relative w-full h-full transition-transform duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
                 {/* Front */}
-                <div className="absolute w-full h-full backface-hidden bg-[#0A1119] border border-cyan-900 rounded-xl p-5 flex flex-col gap-3">
+                <div className="absolute w-full h-full backface-hidden  border border-cyan-900 rounded-xl p-5 flex flex-col gap-3">
                   {card.icon}
                   <p className="text-gray-300 text-sm">{card.title}</p>
                   <p className="text-xl font-bold">{card.bold}</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,19 @@ import BenefitsSection from "../../components/BenefitsSection";
 import Footer from "../../components/Footer";
 import Hero from "../../components/Hero";
 import Navbar from "../../components/Navbar";
+import evolving from "../../assets/icons/evolving.svg";
+import awards from "../../assets/icons/awards.svg";
+import country from "../../assets/icons/countries.svg";
+import trading from "../../assets/icons/trading.svg";
+import Stats from "../../components/Stats";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[#121E2C]">
       <Navbar />
       <Hero />
+      <Stats />
+
       <BenefitsSection />
       <Footer />
     </div>
