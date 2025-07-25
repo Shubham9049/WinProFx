9c844d85a6f224a7bd0f35ae7fd7e6a20a70d683
shubham9049 <shubham9049>
2025-07-25 15:28:56 +0530

    Add changes

--- components/Inside_Contact.tsx
+++ components/Inside_Contact.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
+
 export default function InsightsSection() {
   return (
     <section className="bg-[#072029] text-white py-16 px-4 text-center">
       {/* Heading */}
       <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold leading-tight mb-4">
-        Critical Insights: Unveiling the{" "}
+        Critical Insights: Unveiling the
         <span className="text-[#00CFFF]">Pinnacle Learnings</span> for
         <br className="hidden md:block" /> Strategic Success.
       </h2>
@@ -22,7 +24,7 @@ export default function InsightsSection() {
         rel="noopener noreferrer"
         className="inline-flex items-center gap-2 bg-[#25D366] hover:bg-[#1DA851] text-white px-6 py-2.5 rounded-full text-base font-medium shadow-md border border-[#1DA851] transition"
       >
-        <img
+        <Image
           src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"
           alt="WhatsApp"
           className="w-5 h-5"
