8d71c6bc6b8b8672ed8cd4faf295933fa59594e7
shubham9049 <shubham9049>
2025-08-02 18:48:06 +0530

    Add changes

--- src/app/(dashboard)/live-accounts/page.tsx
+++ src/app/(dashboard)/live-accounts/page.tsx
@@ -222,7 +222,7 @@ export default function LiveAccounts() {
                 Withdraw
               </button>
               <button
-                className="bg-gray-600 px-4 py-2 rounded-md text-sm"
+                className="bg-gray-600 px-4 py-2 rounded-md text-sm cursor-pointer"
                 onClick={() => setShowPasswordModal(true)}
               >
                 Update Password
